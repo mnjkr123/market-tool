c0d132d4a4084c6c01937ed0f74c7c954a56a610
Manoj Kumar <ekmtutor@gmail.com>
2024-08-25 13:35:03 -0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
@@ -21,4 +20,3 @@ export default defineConfig({
     }
   }
 })
-
