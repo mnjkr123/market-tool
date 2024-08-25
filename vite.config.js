12ef3168c3b2385d051bdad22dee87517d6fc0a7
Manoj Kumar <ekmtutor@gmail.com>
2024-08-25 15:50:23 -0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-//Test to check 3:00 PM 08/25
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Root base path for GitHub Pages
   plugins: [react()],
   css: {
     devSourcemap: true
   },
   build: {
-    chunkSizeWarningLimit: 1000, // Increase chunk size warning limit to 1000 kB
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            // Separate vendor code into its own chunk
             return 'vendor';
           }
         }
@@ -22,4 +21,3 @@ export default defineConfig({
     }
   }
 })
-
